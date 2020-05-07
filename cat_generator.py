bfcd245999aad005c735e484162c9d699e57583e
Stefano Rinaldi <stefano.rinaldi@ligo.org>
2020-05-07 10:08:11 +0200

    Added universe generator

---
+++ cat_generator.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+from optparse import OptionParser
+from scipy.special import logsumexp
+from functools import reduce
+from scipy.stats import norm
+import unittest
+import lal
+import cpnest.model
+import sys
+import os
+import readdata
+import matplotlib
+import corner
+import itertools as it
+import cosmology as cs
+import numpy as np
+import likelihood as lk
+import matplotlib.pyplot as plt
+from displaypost import plot_post
+import math
+from schechter import *
+import random as rd
+import lal
+import os
+import sys
+
+def appM(z, M, omega):
+    return M + 5.0*np.log10(1e5*lal.LuminosityDistance(omega,z)) - 5.*np.log10(omega.h)
+
+def gaussian(x, x0, sigma):
+    return np.exp(-(x-x0)**2/(2*sigma**2))/(sigma*np.sqrt(2*np.pi))
+
+
+if __name__ == '__main__':
+
+    omega = lal.CreateCosmologicalParameters(0.7, 0.3, 0.7, -1, 0, 0)
+    M_max    = -4.
+    M_min    = -23.
+    M_cutoff = -12
+    Schechter, alpha, Mstar = SchechterMagFunction(M_min, M_max, omega.h)
+    output = 'mockcatalog2/'
+    if not os.path.exists(output):
+        os.mkdir(output)
+    numberdensity = 0.066
+
+    z_min = 0.002
+    z_max = 0.04
+    dCoVolMax = lal.ComovingVolumeElement(z_max,omega)
+    pM_max    = Schechter(M_max)
+    CoVol = lal.ComovingVolume(omega, z_max) - lal.ComovingVolume(omega, z_min)
+    N_tot = int(CoVol*numberdensity)
+
+    ID      = []
+    ra      = []
+    dec     = []
+    z_cosmo = []
+    z       = []
+    appB    = []
+    absB    = []
+    dB      = []
+    DL      = []
+    host    = []
+
+    ID_h      = []
+    ra_h      = []
+    dec_h     = []
+    z_cosmo_h = []
+    z_h       = []
+    appB_h    = []
+    absB_h    = []
+    dB_h      = []
+    DL_h      = []
+    host_h    = []
+
+
+    for i in range(N_tot):
+
+        sys.stdout.write('{0} out of {1}\r'.format(i+1, N_tot))
+        sys.stdout.flush()
+        ID.append(i)
+        ra.append(rd.uniform(0,2*np.pi))
+        dec.append(rd.uniform(-np.pi/2.,np.pi/2.))
+        while 1:
+            z_temp = rd.uniform(z_min,z_max)
+            if rd.random()*dCoVolMax < lal.ComovingVolumeElement(z_temp,omega):
+                z_c = z_temp
+                z_cosmo.append(z_c)
+                break
+        z_pec = rd.gauss(0, 0.001)
+        z.append(z_c+z_pec)
+        DL.append(lal.LuminosityDistance(omega,z_c))
+        while 1:
+            B_temp = rd.uniform(M_min, M_max)
+            if rd.random()*pM_max < Schechter(B_temp):
+                B = B_temp
+                absB.append(B)
+                break
+        dB.append(0.5)
+        appB.append(appM(z_c, B, omega))
+        host.append(0)
+
+    for i in range(100):
+        while 1:
+            index = rd.randint(0,N_tot-1)
+            if absB[index] < M_cutoff:
+                host[index] = 1
+                ID_h.append(ID[index])
+                ra_h.append(ra[index])
+                dec_h.append(dec[index])
+                z_cosmo_h.append(z_cosmo[index])
+                z_h.append(z[index])
+                appB_h.append(appB[index])
+                absB_h.append(absB[index])
+                dB_h.append(dB[index])
+                DL_h.append(DL[index])
+                host_h.append(host[index])
+                break
+
+    header = 'ID\tra\t\tdec\t\tz\t\tz_cosmo\t\tDL\t\tB_abs\t\tB\t\tB_err\t\thost'
+    fmt = '%d\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%d'
+    np.savetxt(output+'mockcatalog.txt', np.array([ID, ra, dec, z, z_cosmo, DL, absB, appB, dB, host]).T, fmt = fmt, header = header)
+    np.savetxt(output+'hosts.txt', np.array([ID_h, ra_h, dec_h, z_h, z_cosmo_h, DL_h, absB_h, appB_h, dB_h, host_h]).T, fmt = fmt, header = header)
+
+    fig_z_cosmo = plt.figure()
+    fig_z_pm    = plt.figure()
+    fig_M       = plt.figure()
+    fig_sky     = plt.figure()
+
+    ax_z_cosmo  = fig_z_cosmo.add_subplot(111)
+    ax_z_pm     = fig_z_pm.add_subplot(111)
+    ax_M        = fig_M.add_subplot(111)
+
+
+    app_z = np.linspace(z_min, z_max, 1000)
+    app_CoVol = []
+
+
+    for zi in app_z:
+        app_CoVol.append(lal.ComovingVolumeElement(zi, omega)/CoVol)
+
+
+
+
+    app_z_pm = np.linspace(-5*0.001, 5*0.001, 1000)
+    app_M    = np.linspace(M_min, M_max, 1000)
+    app_pM   = []
+
+    for Mi in app_M:
+        app_pM.append(Schechter(Mi))
+
+    ax_z_cosmo.hist(z_cosmo, bins = int(np.sqrt(len(z_cosmo))), density = True)
+    ax_z_cosmo.plot(app_z, app_CoVol)
+    ax_z_cosmo.set_xlabel('$z_{cosmo}$')
+    ax_z_cosmo.set_ylabel('$p(z_{cosmo})$')
+    fig_z_cosmo.savefig(output+'z_cosmo.pdf', bbox_inches='tight')
+
+    ax_z_pm.hist(np.array(z)-np.array(z_cosmo), bins = int(np.sqrt(len(z_cosmo))), density = True)
+    ax_z_pm.plot(app_z_pm, gaussian(app_z_pm, 0, 0.001))
+    ax_z_pm.set_xlabel('$z_{pm}$')
+    ax_z_pm.set_ylabel('$p(z_{pm})$')
+    fig_z_pm.savefig(output+'z_pm.pdf', bbox_inches='tight')
+
+    ax_M.hist(absB, bins = int(np.sqrt(len(absB))), density = True)
+    ax_M.plot(app_M, app_pM)
+    ax_M.set_xlabel('$M\ (B\ band)$')
+    ax_M.set_ylabel('$p(M)$')
+    fig_M.savefig(output+'M.pdf', bbox_inches='tight')
---
+++ generate_events.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import random as rd
+import lal
+import os
+import sys
+
+def LumDist(z, omega):
+    return 3e3*(z + (1-omega.om +omega.ol)*z**2/2.)/omega.h
+
+def dLumDist(z, omega):
+    return 3e3*(1+(1-omega.om+omega.ol)*z)/omega.h
+
+def RedshiftCalculation(LD, omega, zinit=0.3, limit = 0.001):
+    '''
+    Redshift given a certain luminosity, calculated by recursion.
+    Limit is the less significative digit.
+    '''
+    LD_test = LumDist(zinit, omega)
+    if abs(LD-LD_test) < limit :
+        return zinit
+    znew = zinit - (LD_test - LD)/dLumDist(zinit,omega)
+    return RedshiftCalculation(LD, omega, zinit = znew)
+
+file_hosts = './mockcatalog2/hosts.txt'
+
+hosts = np.genfromtxt(file_hosts, names = True)
+omega = lal.CreateCosmologicalParameters(0.7,0.3,0.7,-1,0,0)
+
+omegamin = lal.CreateCosmologicalParameters(0.3,0.3,0.7,-1,0,0)
+omegamax = lal.CreateCosmologicalParameters(2,0.3,0.7,-1,0,0)
+
+
+m_th  = 18.
+
+counter = 1
+
+full_catalog = np.genfromtxt('./mockcatalog2/mockcatalog.txt', names = True)
+
+for gal in hosts:
+    sys.stdout.write('Event {0} of {1}\r'.format(counter, len(hosts)))
+    folder = './mockcatalog2/event_'+str(counter)+'/'
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+    fmt = '%d\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%d'
+    np.savetxt(folder+'host.txt', np.column_stack(list(gal)), fmt=fmt, header = '\t\t'.join(hosts.dtype.names))
+    dra_w  = 0.01
+    ddec_w = 0.01
+    ra_w   = rd.gauss(gal['ra'], dra_w)
+    dec_w  = rd.gauss(gal['dec'], ddec_w)
+    dLD_w  = rd.uniform(0.1,0.3)*gal['DL']
+    LD_w   = rd.gauss(gal['DL'], dLD_w)
+
+    np.savetxt(folder+'posterior.txt', np.column_stack([ra_w, dra_w, dec_w, ddec_w, LD_w, dLD_w]), fmt = '%f\t%f\t%f\t%f\t%f\t%f', header = 'ra\tdra\tdec\tddec\tLD\tdLD')
+
+    LD_max  = LD_w + 2*dLD_w
+    LD_min  = LD_w - 2*dLD_w
+    ra_min  = ra_w - 2*dra_w
+    ra_max  = ra_w + 2*dra_w
+    dec_min = dec_w - 2*ddec_w
+    dec_max = dec_w + 2*ddec_w
+    area = np.pi*dra_w**2
+    volume = (4./3.)*np.pi*(LD_max**3-LD_min**3)*(area/(4*np.pi))
+
+    zmin = RedshiftCalculation(LD_min, omega)
+    zmax = RedshiftCalculation(LD_max, omega)
+
+    np.savetxt(folder+'confidence_region.txt', np.column_stack([ra_min,ra_max,dec_min,dec_max,LD_min,LD_max, zmin, zmax, area*360*180,volume]), fmt = '%f\t\t%f\t\t%f\t\t%f\t\t%f\t\t%f\t\t%f\t\t%f\t\t%f\t\t%f', header = 'ra_min\tra_max\tdec_min\tdec_max\tLD_min\tLD_max\tz_min\tz_max\tarea\tvolume')
+
+    ID      = []
+    ra      = []
+    ra_rad  = []
+    dec     = []
+    dec_rad = []
+    z_cosmo = []
+    z       = []
+    appB    = []
+    absB    = []
+    dB      = []
+    LD      = []
+    host    = []
+
+    for pot_host in full_catalog:
+        if (zmin < pot_host['z'] < zmax) and (np.sqrt((pot_host['ra']-ra_w)**2+(pot_host['dec']-dec_w)**2) < dra_w) and (pot_host['B']<m_th):
+            ID.append(pot_host['ID'])
+            ra.append(np.rad2deg(pot_host['ra']))
+            ra_rad.append(pot_host['ra'])
+            dec.append(np.rad2deg(pot_host['dec']))
+            dec_rad.append(pot_host['dec'])
+            z_cosmo.append(pot_host['z_cosmo'])
+            z.append(pot_host['z'])
+            appB.append(pot_host['B'])
+            absB.append(pot_host['B_abs'])
+            dB.append(pot_host['B_err'])
+            LD.append(pot_host['DL'])
+            host.append(pot_host['host'])
+
+    header = 'ID\tra\t\tra_rad\t\tdec\t\tdec_rad\t\tz\t\tz_cosmo\t\tLD\t\tB_abs\t\tB\t\tB_err\t\thost'
+    fmt = '%d\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%d'
+    np.savetxt(folder+'galaxy_0.9.txt', np.array([ID, ra, ra_rad, dec, dec_rad, z, z_cosmo, LD, absB, appB, dB, host]).T, fmt = fmt, header = header)
+    counter += 1
